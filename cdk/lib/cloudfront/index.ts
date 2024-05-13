305aeeb4b5ad93695e7bb1288771380d5168e175
Or Ben Dahan <or.ben-dahan@risecodes.com>
2024-05-13 16:05:23 +0300

    infra added

--- cdk/lib/cloudfront/index.ts
+++ cdk/lib/cloudfront/index.ts
@@ -2,13 +2,14 @@ import { Construct } from 'constructs';
 import * as cf from 'aws-cdk-lib/aws-cloudfront';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as cfOrigins from 'aws-cdk-lib/aws-cloudfront-origins';
-import { CfnOutput, Duration } from 'aws-cdk-lib';
+import { CfnOutput } from 'aws-cdk-lib';
 import * as acm from 'aws-cdk-lib/aws-certificatemanager';
 import { APP_DOMAIN, APP_DOMAIN_CERT_ARN } from '../const';
-import { CachePolicyProps } from 'aws-cdk-lib/aws-cloudfront/lib/cache-policy';
 
 interface IProps {
     bucket: s3.IBucket,
+    appFolder: string,
+    originAccessIdentity: cf.IOriginAccessIdentity,
 }
 
 export class Cloudfront extends Construct {
@@ -17,34 +18,22 @@ export class Cloudfront extends Construct {
     constructor(scope: Construct, id: string, props: IProps) {
         super(scope, id);
 
-        const originAccessIdentity = new cf.OriginAccessIdentity(this, `${id}-oai`, {
-            comment: id
+        const origin = new cfOrigins.S3Origin(props.bucket, {
+            originAccessIdentity: props.originAccessIdentity,
+            originPath: `/${props.appFolder}`
         });
-        props.bucket.grantRead(originAccessIdentity);
-
-        const cachePolicyProps: CachePolicyProps = {
-            comment: `${id} app cache policy`,
-            queryStringBehavior: cf.CacheQueryStringBehavior.all(),
-            maxTtl: Duration.days(30),
-            minTtl: Duration.days(30),
-            defaultTtl: Duration.days(30),
-            enableAcceptEncodingBrotli: true,
-            enableAcceptEncodingGzip: true
-        };
-
-        const cachePolicy = new cf.CachePolicy(this, `${id}-app-cache-policy`, cachePolicyProps);
 
         this.distribution = new cf.Distribution(this, `${id}-distribution`, {
             comment: APP_DOMAIN,
             domainNames: [APP_DOMAIN],
+            defaultRootObject: 'index.html',
             certificate: acm.Certificate.fromCertificateArn(this, `acm-cert-${APP_DOMAIN}`, APP_DOMAIN_CERT_ARN),
             defaultBehavior: {
-                origin: new cfOrigins.S3Origin(props.bucket, { originAccessIdentity, originPath: `/${id}` }), // BUG - /${id} including the cf suffix
-                viewerProtocolPolicy: cf.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+                origin,
                 compress: true,
-                cachePolicy,
+                cachePolicy: cf.CachePolicy.CACHING_OPTIMIZED,
+                viewerProtocolPolicy: cf.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
             },
-            defaultRootObject: 'index.html',
             errorResponses: [
                 { httpStatus: 404, responseHttpStatus: 200, responsePagePath: '/index.html', },
             ],
--- cdk/lib/const.ts
+++ cdk/lib/const.ts
@@ -1,3 +1,3 @@
-export const APP_BUCKET_NAME = 'static-webz';
+export const APP_BUCKET_NAME = 'static-appz';
 export const APP_DOMAIN = 'redis.sudozone.com';
-export const APP_DOMAIN_CERT_ARN = 'arn:aws:acm:us-east-1:200155604948:certificate/d81b4f4e-41c7-499e-b5e9-a231dcdec36b';
+export const APP_DOMAIN_CERT_ARN = 'arn:aws:acm:us-east-1:200155604948:certificate/20eb5928-26a6-4578-9eed-a34023b1ca41';

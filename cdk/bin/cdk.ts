4f724c44597e012920fa7054050f7bc976245443
Or Ben Dahan <or.ben-dahan@risecodes.com>
2024-05-13 16:18:19 +0300

    infra - wip

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -6,7 +6,9 @@ const app = new cdk.App();
 
 new CdkStack(app, 'redis-cluster-demo', {
   env: {
-    account: process.env.CDK_AWS_ACCOUNT,
-    region: process.env.CDK_AWS_REGION,
+    // account: process.env.CDK_AWS_ACCOUNT,
+    // region: process.env.CDK_AWS_REGION,
+    account: '200155604948',
+    region: 'eu-central-1',
   }
 });

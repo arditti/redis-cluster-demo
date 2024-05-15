26f80bf886277d6a5044b79022d514ce71d4b579
Or Ben Dahan <or.ben-dahan@risecodes.com>
2024-05-15 10:00:51 +0300

    infra - outputs fixed

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -5,14 +5,15 @@ import * as s3Deploy from 'aws-cdk-lib/aws-s3-deployment';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import { Duration } from 'aws-cdk-lib';
+import { BUCKET_ARN_OUTPUT, CF_OAI_ID_OUTPUT, DOMAIN_ACM_ARN_OUTPUT } from './const';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: cdk.StackProps) {
     super(scope, id, props);
 
-    const bucketArn = cdk.Fn.importValue('staticapps-bucket-arn');
-    const oaiId = cdk.Fn.importValue('staticapps-oai-id');
-    const acmArn = cdk.Fn.importValue('sudozone-com-cert-arn');
+    const bucketArn = cdk.Fn.importValue(BUCKET_ARN_OUTPUT);
+    const oaiId = cdk.Fn.importValue(CF_OAI_ID_OUTPUT);
+    const acmArn = cdk.Fn.importValue(DOMAIN_ACM_ARN_OUTPUT);
 
     const bucket = s3.Bucket.fromBucketArn(this, `${id}-bucket`, bucketArn);
     const oai = cloudfront.OriginAccessIdentity.fromOriginAccessIdentityId(this, `${id}-oai`, oaiId);
